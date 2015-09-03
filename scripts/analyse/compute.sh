2a1fd8dafe5cf82ee60d65413ae3ab3a72226ee0
Kadda SAHNINE <ksahnine@inoviabook.local>
2015-09-03 18:10:13 +0200

    Ajout top tweets

--- scripts/analyse/compute.sh
+++ scripts/analyse/compute.sh
@@ -18,6 +18,15 @@ nb_twittos() {
     jq "select(.created_at|match(\"$2\"))|.user.screen_name" $1 | sort -u | wc -l
 }
 
+tab_top_tweets() {
+    if [ $# -lt 2 ]
+    then
+        echo "Exemple : tab_top_tweets \"data/*.json\"  350"
+        exit 1
+    fi
+    (echo "Nb_rtweet\tTexte\tUser"; jq --raw-output "select(.retweeted_status == null and .retweet_count > $2)|[.retweet_count,.text,.user.screen_name] | @tsv" $1 | sort -t$'\t' -k1rn ) | csvlook -d$'\t'
+}
+
 tab_retweeted_users() {
     if [ $# -ne 3 ]
     then
@@ -51,7 +60,7 @@ tab_hashtag() {
         echo "Exemple : tab_hashtag \"data/*.json\" \"Tue Aug 11 2[0-2].*\" 5"
         exit 1
     fi
-    (echo "Nb,Hashtag"; jq ".|select(.created_at|match(\"$2\"))|.entities.hashtags[].text" $1 | tr '[A-Z]' '[a-z]' | tr '[àâäéèêëîïôöùûü]' '[aaaeeeeiioouuu]' | sort | uniq -c | sed -e 's/^ *//;s/ /,/' | sort -rn | head -$3 ) | csvlook
+    (echo "Nb,Hashtag"; jq ".|select(.created_at|match(\"$2\"))|.entities.hashtags[].text" $1 | tr '[A-Z]' '[a-z]' | tr '[àâäéèêëÉîïôöùûüç]' '[aaaeeeeeiioouuuc]' | sort | uniq -c | sed -e 's/^ *//;s/ /,/' | sort -rn | head -$3 ) | csvlook
 }
 
 tab_top_photos() {
@@ -70,6 +79,15 @@ tab_sources() {
         exit 1
     fi
     (echo "Nb,Source"; jq ".|select(.created_at|match(\"$2\"))|.source" $1 | sed 's/"\(<.*>\)\(.*\)\(<.*>\)"/\2/' | sort | uniq -c | sort -rn | head -$3 | sed -e 's/^ *//;s/ /,/' ) | csvlook
+}
+
+csv_accounts_created_at() {
+    if [ $# -ne 2 ]
+    then
+        echo "Exemple : csv_accounts_created_at \"data/*.json\"  \"Tue Aug 11 2[0-2].*\""
+        exit 1
+    fi
+    jq ".|select(.user.created_at|match(\"$2\"))|.user.created_at" $1 | sed 's/"\([A-Z][a-z][a-z]\) \([A-Z][a-z][a-z] [0-9][0-9]\) \(.*\) \([0-9][0-9][0-9][0-9]\)/\2 \4/' | sort | uniq -c | sort -n -k3 | sed 's/^ *//;s/ /,/'
 }
 
 csv_tweets() {
@@ -124,6 +142,40 @@ csv_graph_sources() {
     jq ".|select(.source|match(\"Twitter Web Client\"))|.created_at" $1 | xargs -I ? date -j -f "%a %h %d %H:%M:%S %z %Y" "?" "+%Y-%m-%d_%H:00:00" | sort | uniq -c | sed -e 's/^ *//;s/ /,/' | awk -F"," '{ print $2 "," $1}' > web.csv
 
     echo "Generation des fichiers iphone.csv, android.csv, web.csv"
+}
+
+all_stats() {
+    echo "** Nb tweets **"
+    NbTweets=`nb_tweets "$1" ".*"`
+    echo $NbTweets
+    echo ""
+    echo "** Nb utilisateurs **"
+    NbTwittos=`nb_twittos "$1" ".*"`
+    echo $NbTwittos
+    echo ""
+    echo "** Nb tweets/user **"
+    echo "scale=2; $NbTweets / $NbTwittos" | bc
+    echo ""
+    echo "** Top tweets **"
+    tab_top_tweets "$1" 300
+    echo ""
+    echo "** Les 5 photos les plus diffusées **"
+    tab_top_photos "$1" 5
+    echo ""
+    echo "** Les 4 canaux principaux **"
+    tab_sources "$1" ".*" 4
+    echo ""
+    echo "** Les 20 comptes les plus retweetes **"
+    tab_retweeted_users "$1" ".*" 20
+    echo ""
+    echo "** Les 20 comptes ayant le plus tweeté **"
+    tab_tweeted_users "$1" ".*" 20
+    echo ""
+    echo "** Les 20 hashtags les plus utilisés **"
+    tab_hashtag "$1" ".*" 20
+    echo ""
+    echo "** Les 20 comptes les plus mentionnés **"
+    tab_mentions "$1" ".*" 20
 }
 
 usage() {
